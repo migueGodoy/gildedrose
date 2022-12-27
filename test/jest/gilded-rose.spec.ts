67f6d2248fa12b31377a134070628b0294b05d4b
Migue Godoy <migue@MacBook-Pro-de-admin-2.local>
2022-12-27 15:39:52 +0100

    Add more tests

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -2,10 +2,16 @@ import { GildedRose } from '@/gilded-rose';
 import { Item } from "@/Item";
 
 describe('Gilded Rose', () => {
-  it('should decrement quality by one for normal items', () => {
+  it('should decrease quality by one for normal items', () => {
     const gildedRose = new GildedRose([new Item('foo', 4, 4)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(3);
+  });
+
+  it('should increase quality by 2 for AGED BRIE if sellIn falls below 0', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(6);
   });
 
   it('should increment quality by 3 for conctert pass with sellIn < 6 and quality < 50', () => {
@@ -13,5 +19,12 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([new Item(itemName, 4, 4)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(7);
+  });
+
+  it('should increment quality by 2 for conctert pass with sellIn between 6 and 11 and quality < 50', () => {
+    const itemName = 'Backstage passes to a TAFKAL80ETC concert';
+    const gildedRose = new GildedRose([new Item(itemName, 10, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(6);
   });
 });
