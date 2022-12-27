e48ec09081458e0d2888b288fef2fee393f6cad4
Migue Godoy <migue@MacBook-Pro-de-admin-2.local>
2022-12-27 08:04:43 +0100

    Add a test for concert ticket

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -7,4 +7,11 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(3);
   });
+
+  it('should increment quality by 3 for conctert pass with sellIn < 6 and quality < 50', () => {
+    const itemName = 'Backstage passes to a TAFKAL80ETC concert';
+    const gildedRose = new GildedRose([new Item(itemName, 4, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(7);
+  });
 });
